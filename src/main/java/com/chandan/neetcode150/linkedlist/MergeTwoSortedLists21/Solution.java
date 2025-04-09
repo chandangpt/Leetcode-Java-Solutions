644e9167365c4a1165deb246b3f363504a76ef9b
chandangupta <chandan.gpt10@gmail.com>
2025-04-09 21:19:46 +0530

    Added code

--- src/main/java/com/chandan/neetcode150/linkedlist/MergeTwoSortedLists21/Solution.java
+++ src/main/java/com/chandan/neetcode150/linkedlist/MergeTwoSortedLists21/Solution.java
@@ -22,4 +22,24 @@ class Solution {
             return list2;
         }
     }
+
+    /*public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode();
+        ListNode temp = head;
+        while(list1 != null && list2 != null) {
+            if(list1.val < list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp = temp.next;
+        }
+        if(list1 != null)
+            temp.next = list1;
+        if(list2 != null)
+            temp.next = list2;
+        return head.next;
+    }*/
 }
