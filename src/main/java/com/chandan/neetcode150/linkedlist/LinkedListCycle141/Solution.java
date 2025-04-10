556d1564a90b3a6494a75f8373d0dca4775bd39f
chandangupta <chandan.gpt10@gmail.com>
2025-04-10 22:43:16 +0530

    Added code

---
+++ src/main/java/com/chandan/neetcode150/linkedlist/LinkedListCycle141/Solution.java
@@ -0,0 +1,24 @@
+package com.chandan.neetcode150.linkedlist.LinkedListCycle141;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow, fast;
+        slow = fast = head;
+        while((fast != null) && (fast.next != null)) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
+                return true;
+        }
+        return false;
+    }
+}
