872db440b800224bf8036e6a41f9e97eb9bc24de
chandangupta <chandan.gpt10@gmail.com>
2025-04-28 21:41:41 +0530

    Added code

---
+++ src/main/java/com/chandan/takeUforward/placementseries/SortColors75/Solution.java
@@ -0,0 +1,25 @@
+package com.chandan.takeUforward.placementseries.SortColors75;
+
+public class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+        while(mid <= high) {
+            if(nums[mid] == 0) {
+                swap(nums, mid, low);
+                mid++;
+                low++;
+            } else if(nums[mid] == 1) {
+                mid++;
+            } else if(nums[mid] == 2) {
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+
+    void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
