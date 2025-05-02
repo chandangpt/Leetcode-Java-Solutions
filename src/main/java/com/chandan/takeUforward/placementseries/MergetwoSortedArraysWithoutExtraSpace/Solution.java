122cddb0ef0a9d035feefb5265abdc04b820b193
chandangupta <chandan.gpt10@gmail.com>
2025-05-03 00:35:39 +0530

    Added code

---
+++ src/main/java/com/chandan/takeUforward/placementseries/MergetwoSortedArraysWithoutExtraSpace/Solution.java
@@ -0,0 +1,121 @@
+package com.chandan.takeUforward.placementseries.MergetwoSortedArraysWithoutExtraSpace;
+
+import java.util.Arrays;
+
+/*
+* Problem statement: Given two sorted arrays arr1[] and arr2[] of sizes n and m in non-decreasing order. Merge them in sorted order. Modify arr1 so that it contains the first N elements and modify arr2 so that it contains the last M elements.
+
+Examples
+
+Example 1:
+
+Input:
+n = 4, arr1[] = [1 4 8 10]
+m = 5, arr2[] = [2 3 9]
+
+Output:
+arr1[] = [1 2 3 4]
+arr2[] = [8 9 10]
+
+Explanation:
+After merging the two non-decreasing arrays, we get, 1,2,3,4,8,9,10.
+
+Example2:
+
+Input:
+n = 4, arr1[] = [1 3 5 7]
+m = 5, arr2[] = [0 2 6 8 9]
+
+Output:
+arr1[] = [0 1 2 3]
+arr2[] = [5 6 7 8 9]
+
+Explanation:
+After merging the two non-decreasing arrays, we get, 0 1 2 3 5 6 7 8 9.
+* */
+public class Solution {
+    public static void swapIfGreater(long[] arr1, long[] arr2, int ind1, int ind2) {
+        if (arr1[ind1] > arr2[ind2]) {
+            long temp = arr1[ind1];
+            arr1[ind1] = arr2[ind2];
+            arr2[ind2] = temp;
+        }
+    }
+
+    public static void mergeUsingGapAlgo(long[] arr1, long[] arr2, int n, int m) {
+
+        // len of the imaginary single array:
+        int len = n + m;
+
+        // Initial gap:
+        int gap = (len / 2) + (len % 2);
+
+        while (gap > 0) {
+            // Place 2 pointers:
+            int left = 0;
+            int right = left + gap;
+            while (right < len) {
+                // case 1: left in arr1[]
+                //and right in arr2[]:
+                if (left < n && right >= n) {
+                    swapIfGreater(arr1, arr2, left, right - n);
+                }
+                // case 2: both pointers in arr2[]:
+                else if (left >= n) {
+                    swapIfGreater(arr2, arr2, left - n, right - n);
+                }
+                // case 3: both pointers in arr1[]:
+                else {
+                    swapIfGreater(arr1, arr1, left, right);
+                }
+                left++; right++;
+            }
+            // break if iteration gap=1 is completed:
+            if (gap == 1) break;
+
+            // Otherwise, calculate new gap:
+            gap = (gap / 2) + (gap % 2);
+        }
+    }
+
+    public static void merge(long[] arr1, long[] arr2, int n, int m) {
+
+        // Declare 2 pointers:
+        int left = n - 1;
+        int right = 0;
+
+        // Swap the elements until arr1[left] is
+        // smaller than arr2[right]:
+        while (left >= 0 && right < m) {
+            if (arr1[left] > arr2[right]) {
+                long temp = arr1[left];
+                arr1[left] = arr2[right];
+                arr2[right] = temp;
+                left--;
+                right++;
+            } else {
+                break;
+            }
+        }
+
+        // Sort arr1[] and arr2[] individually:
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+    }
+    public static void main(String[] args) {
+        long[] arr1 = {1, 4, 8, 10};
+        long[] arr2 = {2, 3, 9};
+        int n = 4, m = 3;
+        merge(arr1, arr2, n, m);
+        System.out.println("The merged arrays are:");
+        System.out.print("arr1[] = ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr1[i] + " ");
+        }
+        System.out.print("\narr2[] = ");
+        for (int i = 0; i < m; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+        System.out.println();
+    }
+}
