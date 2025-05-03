d9f6ab8910acac260bde6dbb4f42d50a70ef1663
chandangupta <chandan.gpt10@gmail.com>
2025-05-03 19:46:37 +0530

    Added code

--- src/main/java/com/chandan/takeUforward/placementseries/SetMatrixZeroes73/Solution.java
+++ src/main/java/com/chandan/takeUforward/placementseries/SetMatrixZeroes73/Solution.java
@@ -1,5 +1,8 @@
 package com.chandan.takeUforward.placementseries.SetMatrixZeroes73;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
@@ -28,5 +31,26 @@ public class Solution {
         if(rowZero == true)
             for(int i = 0; i < n; i++)
                 matrix[0][i] = 0;
+    }
+
+    public void setZeroes1(int[][] matrix) {
+        Set<Integer> rowZero = new HashSet<>();
+        Set<Integer> colZero = new HashSet<>();
+        int m = matrix.length, n = matrix[0].length;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++)
+                if(matrix[i][j] == 0) {
+                    rowZero.add(i);
+                    colZero.add(j);
+                }
+        }
+        for(int row : rowZero) {
+            for(int i = 0; i < n; i++)
+                matrix[row][i] = 0;
+        }
+        for(int col : colZero) {
+            for(int i = 0; i < m; i++)
+                matrix[i][col] = 0;
+        }
     }
 }
