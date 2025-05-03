59ad166e48c7ca2aa29bf9635faa4ee22c8fc94c
chandangupta <chandan.gpt10@gmail.com>
2025-05-03 18:37:17 +0530

    Added code

--- src/main/java/com/chandan/takeUforward/placementseries/MaximumSubarray53/Solution.java
+++ src/main/java/com/chandan/takeUforward/placementseries/MaximumSubarray53/Solution.java
@@ -14,4 +14,16 @@ public class Solution {
         }
         return maxSum;
     }
+
+    public int maxSubArray1(int[] nums) {
+        int maxSum = nums[0], currSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            currSum += nums[i];
+            maxSum = Math.max(maxSum, currSum);
+            if(currSum < 0) {
+                currSum = 0;
+            }
+        }
+        return maxSum;
+    }
 }
